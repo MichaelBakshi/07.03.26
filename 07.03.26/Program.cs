91b912184062bf766cedb5f5b504a15bb12a0e93
Michael Bakshi <michael.bakshi83@gmail.com>
2021-03-11 19:55:37 +0200

    Add project files.

---
+++ 07.03.26/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07._03._26
+{
+    class Program
+    {
+        public static int GetSum(int a, int b)
+        {
+            return a + b;
+        }
+        static void Main(string[] args)
+        {
+
+            Task<int> task1 = Task<int>.Factory.StartNew(() =>
+            GetSum(4,5)
+            );
+            int result = task1.Result;
+            Console.WriteLine(result);
+
+        }
+    }
+}
